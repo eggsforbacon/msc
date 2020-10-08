d55466a926e88da4ffbc2880db71747b8dfbde7c
Samuel Hernandez <doicirohangnan@gmail.com>
2020-10-08 15:58:10 -0500

    Initialized repo and added welcome screen

---
+++ src/model/Operations.java
@@ -0,0 +1,28 @@
+package model;
+import ui.*;
+import java.io.IOException;
+
+public class Operations {
+  public static void clrscm() {
+    try {
+      final String OS = System.getProperty("os.name");
+
+        if (OS.contains("Windows")) {
+          new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+        }
+        else {
+          Runtime.getRuntime().exec("clear");
+        }
+    }catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void queue(int millis) {
+    try {
+      Thread.sleep(millis);
+    }catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,12 @@
+package ui;
+import model.*;
+import java.util.*;
+
+public class Main {
+
+  public static final String VERSION = "0.1-pre";
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    MainMenu.welcomeLogo(35,in);
+  }
+}
